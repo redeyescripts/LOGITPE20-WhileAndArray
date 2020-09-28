ec18348a6c828331b284437a1cfe831511e2d33c
Opilane TTHK <opilane@tthk.ee>
2020-09-28 14:51:25 +0300

    first commit

---
+++ GuessMyColor/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace GuessMyColor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int ComputerNumber = rnd.Next(1, 4);
+
+
+            bool rightGuess = false;
+
+            //  int userGuess = Int32.Parse(Console.ReadLine());
+            while (!rightGuess)
+            {
+                Console.WriteLine("Whats my color?");
+                string userInput = Console.ReadLine().ToLower();
+
+
+
+
+
+                if (ComputerNumber == 1 && userInput == "red")
+                {
+                    Console.WriteLine("You won!");
+                    rightGuess = true;
+
+
+                }
+                else if (ComputerNumber == 2 && userInput == "green")
+                {
+                    Console.WriteLine("You won!");
+                    rightGuess = true;
+
+                }
+                else if (ComputerNumber == 3 && userInput == "blue")
+                {
+                    Console.WriteLine("You won!");
+                    rightGuess = true;
+
+                }else
+                {
+                    Console.WriteLine("Try again :(");
+                } 
+            }
+        }
+    }
+}
+
+
+
---
+++ WhileAndArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace WhileAndArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while(i != 11)
+            {
+                Console.WriteLine(i);
+                    i++;
+               
+                
+            }
+
+            int k = 10;
+            while(k != 0)
+            {
+                Console.WriteLine(k);
+                k--;
+            }
+           
+
+            
+        }
+    }
+}
